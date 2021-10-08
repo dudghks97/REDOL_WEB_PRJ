60714b5a94d3afffdf59fea468440c26cd500295
dudghks97 <yh9638@skuniv.ac.kr>
2021-10-08 17:28:16 +0900

    21.10.08_17:28_Choi

--- 02_Proejct/REDOL/src/main/java/com/test/redol/config/mvcConfiguration.java
+++ 02_Proejct/REDOL/src/main/java/com/test/redol/config/mvcConfiguration.java
@@ -14,9 +14,9 @@ public class mvcConfiguration implements WebMvcConfigurer {
     public void addResourceHandlers(final ResourceHandlerRegistry registry) {
         // 1번째 라인 : "/" 를 시작으로 하는 모든 요청을 다룬다는 의미
         // 2번째 라인 : 1번에 해당하는 요청을 처리할 자원을 찾을 위치(여기서는 정적파일위치)
-        // 3번째 라인 : 요청에 대한 캐시를 5분으로 설정한 것
+        // 3번째 라인 : 요청에 대한 캐시 갱신 시간을 설정한 것
         registry.addResourceHandler("resources/**")
                 .addResourceLocations("classpath:/static/")
-                .setCacheControl(CacheControl.maxAge(5, TimeUnit.MINUTES));
+                .setCacheControl(CacheControl.maxAge(30, TimeUnit.SECONDS));
     }
 }
--- 02_Proejct/REDOL/src/main/java/com/test/redol/controller/PageController.java
+++ 02_Proejct/REDOL/src/main/java/com/test/redol/controller/PageController.java
@@ -9,8 +9,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
+    // about 페이지
     @GetMapping("/about")           // url 매핑
     public String about() {         // page 이름
         return "about";             // 연결할 html 파일의 이름 .html 을 제외해야함
+    }
+
+    // services 페이지
+    @GetMapping("/services")
+    public String services() {
+        return "services";
+    }
+
+    // blog 페이지
+    @GetMapping("/blog")
+    public String blog() {
+        return "blog";
+    }
+
+    // contact 페이지
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
     }
 }
