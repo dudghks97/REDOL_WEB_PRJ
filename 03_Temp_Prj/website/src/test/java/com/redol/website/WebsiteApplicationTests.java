4e9493e4edab5455f7bf045beab8d36f39bc684d
dudghks97 <yh9638@skuniv.ac.kr>
2021-10-15 15:40:20 +0900

    21.10.15_Choi

---
+++ 03_Temp_Prj/website/src/test/java/com/redol/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redol.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
